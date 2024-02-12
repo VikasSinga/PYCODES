daec0698b7a5979553dc21320a7a8aa91995e527
VikasSinga <singamsettivikas666@gmail.com>
2024-02-12 09:19:23 +0530

    new change

---
+++ Grade Calculator.py
@@ -0,0 +1,12 @@
+Score = int(input())
+
+if Score >= 90:
+    print("A Grade")
+elif Score >= 80 :
+    print("B Grade")
+elif Score >= 70 :
+    print("C Grade")
+elif Score >=60:
+    print("D Grade")
+else:
+    print("E Grade")
